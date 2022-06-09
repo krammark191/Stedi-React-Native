043998934f1e6884c24ed2b6a31431acfbbaee00
Mark Van Horn <krammark191@gmail.com>
2022-06-09 13:04:25 -0600

    added Login.js

---
+++ Login.js
@@ -0,0 +1,38 @@
+import {useState, useRef} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("Useless Text");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder = "555-555-5555"
+        keyboardType = 'numeric'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={oneTimePassword}
+        placeholder="1234"
+        keyboardType="numeric"
+        secureTextEntry={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default Login;
