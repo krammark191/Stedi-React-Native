aadb98f537fea18bc879a6f6839efba17f37d011
Mark Van Horn <krammark191@gmail.com>
2022-06-09 20:04:53 -0600

    i dont know

--- Login.js
+++ Login.js
@@ -1,38 +1,41 @@
-import {useState, useRef} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { useState, useRef } from 'react'
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native'
 
 const Login = () => {
-  const [phoneNumber, setPhoneNumber] = useState("Useless Text");
-  const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [phoneNumber, setPhoneNumber] = useState('Useless Text')
+  const [oneTimePassword, setOneTimePassword] = useState(null)
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.margin}>
       <TextInput
         style={styles.input}
         onChangeText={setPhoneNumber}
         value={phoneNumber}
-        placeholder = "555-555-5555"
-        keyboardType = 'numeric'
+        placeholder='555-555-5555'
+        keyboardType='numeric'
       />
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
-        placeholder="1234"
-        keyboardType="numeric"
+        placeholder='1234'
+        keyboardType='numeric'
         secureTextEntry={true}
       />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
-    padding: 10,
+    padding: 10
   },
-});
+  margin: {
+    marginTop: 100
+  }
+})
 
-export default Login;
+export default Login
